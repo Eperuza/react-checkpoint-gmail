6d0c4a61fbe3215ba1c4ce782b8e51681faed46e
Gregory Oladipo <gregory.oladipo@galvanize.com>
2021-03-09 14:31:05 -0700

    add encapsulation

---
+++ Encapsulation/encapsulation.js
@@ -0,0 +1,22 @@
+/* 
+The Mission, Operation "Key Maker": 
+Steven Richards(USAF) & Ethan Wise(USAF) are on special assignment 
+operating out of Alexandria and Aswan, Egypt. There they will train
+Egyptian military personnel in digital counter-terrorism
+with cellist and beverage connoisseur, Zach Mansell, as a civilian attachment.
+
+The trio are posing as bourgeois, Australo-British, millionaire playboys, who struck it rich
+selling their own line of niche, novelty, boiled, flavorless, bottled, Japanese, spring waters
+called "Z Water". As a cover, they have been assigned a beach house in Alexandria where they host
+occasional, lavish parties DJ'd by "TJ"(ret. Army), a U.S. expat and bilingual tour-guide.
+
+It is your job as a class to implement security for their beach-side
+base of operations. Godspeed.
+*/
+
+// I do -> 1. Implement `public` areas. The entire house is open. Not good. Not fun.
+// We do -> 2a. Implement `private` areas with "Conventional Privacy"
+// We do -> 2b. Implement `private` areas with "Closure Privacy"
+// We do -> 2c. Implement `private` with ES6 getters and setters
+// We do -> 3. Implement `WeakMap` for `private` and use its get and setters
+// You(Two) do -> 4. Implement `protected` areas using `WeakMap` (this is purposefully challenging)
