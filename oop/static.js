af397b30f2ffd6d903d5cfd710e9321d152cc3b1
Jeff Haddock <jeff.haddock.developer@gmail.com>
2021-03-12 15:30:49 -0500

    added old OOP checkpoint as well as tests!

--- oop/static.js
+++ oop/static.js
@@ -1,11 +1,19 @@
 class Pet {
-    constructor(givenAnimalType, name, speed){
+    constructor(givenAnimalType, name, speed, noise){
         this.type = givenAnimalType;
         this.name = name;
         this.speed = speed;
+        this.noise = noise;
     }
-
-   
+        
+    makeAnimalSound(additionalSound){
+        console.log(this.noise + additionalSound)
+    }
+    
+    // makeAnimalSound(){
+    //     console.log(this.noise);
+    // }
+    
 
     static whichAnimalIsFaster(pet1, pet2){
         if(pet1.speed > pet2.speed)
@@ -17,14 +25,17 @@ class Pet {
 
 class Dog extends Pet {
     constructor(name){
-        super("Dog", name, 20)
+        super("Dog", name, 20, "Aroof");
     }
-
 }
 
 class Cheetah extends Pet{
     constructor(name){
-        super("Cheetah", name, 70);
+        super("Cheetah", name, 70, "Rawr");
+    }
+
+    makeAnimalSound() {
+        console.log("Cheetah Sounds!!");
     }
 }
 
@@ -36,11 +47,14 @@ class Cheetah extends Pet{
 let myDogDoug = new Dog("Doug");
 let myDog2 = new Dog("Fido");
 
-let myCheetah = new Pet("Cheetah", "Charlie", 70);
+let myCheetah = new Cheetah("Charlie");
 let myCheetah2 = new Cheetah("Casey")
 
+myDogDoug.makeAnimalSound(" and Ruff");
+myCheetah.makeAnimalSound();
+
 //ClassName.FunctionInvocation
-console.log(Pet.whichAnimalIsFaster(myDog2, myCheetah));
+//console.log(Pet.whichAnimalIsFaster(myDog2, myCheetah));
 
 //myCheetah.name = "Charlie";
 //console.log(myCheetah.type, myCheetah.name);
